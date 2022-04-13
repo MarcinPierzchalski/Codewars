b4d1894e660984c158db19be06a000f3a2ee5207
Marcin P <mpierzchalski@gmail.com>
2022-04-13 20:52:48 +0200

    rot13v2

---
+++ rot13.js
@@ -0,0 +1,12 @@
+function rot13(message){
+
+    return message.replace(/[A-Z]/gi, c =>
+      "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"[
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".indexOf(c) ] )
+  }
+
+function rot13(message) {
+var a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+return message.replace(/[a-z]/gi, c => b[a.indexOf(c)])
+}
