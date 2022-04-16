e8748e777710a3ced846f798d55cd399f24c3b0c
Marcin P <mpierzchalski@gmail.com>
2022-04-16 22:39:54 +0200

    arrayOrder2

--- ArrayOrder.js
+++ ArrayOrder.js
@@ -1,23 +1,22 @@
 function order(words){
     let arr = words.split(' ')
-    console.log(arr)
-    
     let res = []
+    let count = 1
+
     while(arr.length !==0){
-        console.log(arr)
-      for(let i=1;i<=arr.length; i++){
-        test  = arr[i-1].replace(/[^0-9]/g, "")
-        // console  2.log(test)
-        if(i-1 === test){
-            // console.log(arr)
-            res.push(arr[i-1])
-            arr.splice(i-1,1)
-            i--
-            console.log(res)
+        for(let i=0;i<arr.length;i++){
+            if(count == arr[i].replace(/[^0-9]/g, "")) {
+                res.push(arr[i]);
+                count++;
+                arr.splice(i,1);
+                i--;
+    
+                // console.log(arr);
+            }
+
         }
-      }
     }
-    // console.log(res)
+    console.log(res.join(' '))
   }
 
   order("is2 Thi1s T4est 3a")
