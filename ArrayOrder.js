46750611f4b0109d87f0cfee0a436fca8c53cd5e
Marcin P <mpierzchalski@gmail.com>
2022-04-17 20:58:27 +0200

    arrayOrder_done

--- ArrayOrder.js
+++ ArrayOrder.js
@@ -1,22 +1,36 @@
-function order(words){
-    let arr = words.split(' ')
-    let res = []
-    let count = 1
+// function order(words){
+//     let arr = words.split(' ')
+//     let res = []
+//     let count = 1
 
-    while(arr.length !==0){
-        for(let i=0;i<arr.length;i++){
-            if(count == arr[i].replace(/[^0-9]/g, "")) {
-                res.push(arr[i]);
-                count++;
-                arr.splice(i,1);
-                i--;
+//     while(arr.length !==0){
+//         for(let i=0;i<arr.length;i++){
+//             if(count == arr[i].replace(/[^0-9]/g, "")) {
+//                 res.push(arr[i]);
+//                 count++;
+//                 arr.splice(i,1);
+//                 i--;
     
-                // console.log(arr);
-            }
+//                 // console.log(arr);
+//             }
+//         }
+//     }
+//     return(res.join(' '))
+//   }
 
-        }
-    }
-    console.log(res.join(' '))
+//   console.log(order("4of Fo1r pe6ople g3ood th5e the2"))
+
+function compareNumbers(a, b) {
+    return a.replace(/[^0-9]/g, "") - b.replace(/[^0-9]/g, "");
   }
 
-  order("is2 Thi1s T4est 3a")
+function order(words){
+    let arr = words.split(' ')
+    arr.sort(compareNumbers)
+    console.log(arr.join(' '))
+}
+
+
+ order("fS4of Fo1r pe6ople g3ood th5e the2")
+
+ 
