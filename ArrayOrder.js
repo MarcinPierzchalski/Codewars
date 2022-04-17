5ef009c0dd2e2a3775e5d6fa5f118daeadc42911
Marcin P <mpierzchalski@gmail.com>
2022-04-17 22:16:51 +0200

    arrayOrder_done_add

--- ArrayOrder.js
+++ ArrayOrder.js
@@ -33,4 +33,12 @@ function order(words){
 
  order("fS4of Fo1r pe6ople g3ood th5e the2")
 
- 
+function order(words){
+
+return words.split(' ').sort(function(a, b){
+    return a.match(/\d/) - b.match(/\d/);
+    }).join(' ');
+}
+
+
+
