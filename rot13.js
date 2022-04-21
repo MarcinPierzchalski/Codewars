1e61a54258f259a698ed1adc2910d913b6d0616e
Marcin P <mpierzchalski@gmail.com>
2022-04-21 12:50:55 +0200

    ROT13V2

--- rot13.js
+++ rot13.js
@@ -10,3 +10,10 @@ var a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
 return message.replace(/[a-z]/gi, c => b[a.indexOf(c)])
 }
+
+function rot13(str) {
+  return str.replace(/[a-z]/ig, function(x){
+    return String.fromCharCode(x.charCodeAt(0) + (x.toLowerCase() <= 'm' ? 13: -13));
+  });
+}
+
