eb820b399a5602a60a75015e3c626d2a9f62f1d1
Marcin P <mpierzchalski@gmail.com>
2022-04-22 16:31:59 +0200

    snail

--- Scramblings.js
+++ Scramblings.js
@@ -1,9 +1,2 @@
 function scramble(str1, str2) {
-    let array1 = str1.split('')
-    let array2 = str2.split('')
-    console.log(array2)
-    if (array1.length === array2.length && array1.every(function(value, index) { return value === array2[index]})) return true
-    else return false
-  }
-
-  scramble('rkqodlw','world')
+}
--- SnailSort.js
+++ SnailSort.js
@@ -1,9 +1,36 @@
 function snail(array){
-    
-
+    let result = [];
+    let row = 0;
+    let col = 0;
+    let dir = 'right';
+    let maxRow = array.length - 1;
+    let maxCol = array[0].length - 1;
+    while (row <= maxRow && col <= maxCol) {
+        if (dir === 'right') {
+        for (let i = col; i <= maxCol; i++) {
+            result.push(array[row][i]);
+        }
+        row++;
+        dir = 'down';
+        } else if (dir === 'down') {
+        for (let i = row; i <= maxRow; i++) {
+            result.push(array[i][maxCol]);
+        }
+        maxCol--;
+        dir = 'left';
+        } else if (dir === 'left') {
+        for (let i = maxCol; i >= col; i--) {
+            result.push(array[maxRow][i]);
+        }
+        maxRow--;
+        dir = 'up';
+        } else if (dir === 'up') {
+        for (let i = maxRow; i >= row; i--) {
+            result.push(array[i][col]);
+        }
+        col++;
+        dir = 'right';
+        }
+    }
+    return result;
 }
-
-
-
-
-snail([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
