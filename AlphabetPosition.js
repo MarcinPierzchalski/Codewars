58db60335cf235b3f7ac4e39150676d8d81d37da
Marcin P <mpierzchalski@gmail.com>
2022-04-28 19:40:05 +0200

    AlphabetPositionDONEgit add .

--- AlphabetPosition.js
+++ AlphabetPosition.js
@@ -1,4 +1,14 @@
-function alphabetPosition(text) {
-  // reminder to add code here...
-    return text;
+
+  function alphabetPosition(text) {
+  
+    arr = text.split('')
+    console.log(arr)
+    arrnum = []
+    for(i=0;i<arr.length;i++){
+      if(arr[i].toLowerCase().match(/[a-z]/i)){
+      num = arr[i].toLowerCase().charCodeAt(0) - 96
+      if(num>0) arrnum.push(num)
+    }}
+    return(arrnum.join(' '))
+  //   return text;
   }
