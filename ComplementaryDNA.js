20466deaf5a8fa35010698ca5e2b0bb31c8a817e
Marcin P <mpierzchalski@gmail.com>
2022-04-30 20:51:35 +0200

    DNAdoneOther

--- ComplementaryDNA.js
+++ ComplementaryDNA.js
@@ -12,3 +12,22 @@ function DNAStrand(dna){
     
   
   }
+
+function DNAStrand(dna) {
+return dna.replace(/./g, function(c) {
+    return DNAStrand.pairs[c]
+})
+}
+
+DNAStrand.pairs = {
+A: 'T',
+T: 'A',
+C: 'G',
+G: 'C',
+}
+
+var pairs = {'A':'T','T':'A','C':'G','G':'C'};
+
+function DNAStrand(dna){
+return dna.split('').map(function(v){ return pairs[v] }).join('');
+}
